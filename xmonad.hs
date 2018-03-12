4b6ae13b1881b4af9e2c3af488c20596ec766c45
Daniel Vianna <dmlvianna@gmail.com>
2018-03-12 21:07:23 +1100

    about to add turtle

--- xmonad.hs
+++ xmonad.hs
@@ -1,16 +1,21 @@
 
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run (spawnPipe)
-import XMonad.Util.EZConfig (additionalKeys)
-import System.IO
-import System.Taffybar.Hooks.PagerHints (pagerHints)
+import           System.IO
+import           System.Process
+import           System.Taffybar.Hooks.PagerHints (pagerHints)
+import           XMonad
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Util.EZConfig             (additionalKeys)
+import           XMonad.Util.Run                  (spawnPipe)
 
-main = do 
+
+
+main = do
   _ <- spawnPipe "xautolock -time 10 -locker slock"
   _ <- spawnPipe "taffybar"
-  _ <- spawnPipe "wpa_gui -t"
+  -- _ <- case readProcess "pgrep" ["-x"] "wpa_gui" of
+  --       Nothing -> spawnPipe "wpa_gui -t"
+  --       _ -> mempty
   xmonad . docks . pagerHints $ def
     { manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts $ layoutHook defaultConfig
@@ -19,7 +24,7 @@ main = do
     , focusedBorderColor = "#cd8b00"
     , modMask = mod4Mask -- depends on ~/.Xmodmap assigning Alt_R to mod3
     } `additionalKeys`
-    [ 
+    [
     -- lock screen
     ((mod4Mask .|. shiftMask, xK_z), spawn "xautolock -locknow")
 
