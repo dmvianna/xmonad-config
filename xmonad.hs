b8466a53662a3d0b405c5d7bc78d88d8435860ec
Daniel Vianna <dmlvianna@gmail.com>
2018-03-13 10:16:35 +1100

    moved programs to ~/.xprofile

--- xmonad.hs
+++ xmonad.hs
@@ -1,10 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
--- import qualified Data.Text                        as T
 import           System.IO
 import           System.Process
 import           System.Taffybar.Hooks.PagerHints (pagerHints)
--- import qualified Turtle                           as Turtle
 import           XMonad
 import           XMonad.Hooks.DynamicLog
 import           XMonad.Hooks.ManageDocks
@@ -12,18 +10,7 @@ import           XMonad.Hooks.ManageHelpers
 import           XMonad.Util.EZConfig             (additionalKeys)
 import           XMonad.Util.Run                  (spawnPipe)
 
-
--- conditionalPipe :: T.Text -> T.Text -> Handle
--- conditionalPipe p args =
---   case Turtle.proc "pgrep" ["-x", p] 0 of
---     Turtle.ExitFailure _ ->
---       spawnPipe $ T.pack (T.concat [p " " args])
---     Turtle.ExitSuccess   -> pure mempty
-
 main = do
-  _ <- spawnPipe "xautolock -time 10 -locker slock"
-  _ <- spawnPipe "taffybar"
-  _ <- spawnPipe "wpa_gui -t"
   xmonad . docks . pagerHints $ def
     { manageHook = manageDocks
                    <+> myManageHook
